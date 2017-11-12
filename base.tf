04649ca6a0a0b6c374ce943b4307a53b81e4f6cf
Tom Monte <tmonte@gmail.com>
2017-11-12 13:39:54 -0500

    Update base.tf

--- base.tf
+++ base.tf
@@ -1,27 +1,11 @@
-/*
-This file uses environment varialbes for the access and secret keys.
-The ami image is the free tier Unbunti 16.04 AMI.
-*/
-
 provider "aws" {
-  region = "us-east-1"
   shared_credentials_file = "/home/tmonte/.aws/credentials"
+  region     = "us-east-1"
 }
 
-# Create a VPC
-module "vpc" {
-  name = "sparknet"
-  cidr = "10.0.0.0/16"
-  public_subnet = "10.1.1.0/24"
-}
-
-
-# Sets up the base instance
-resource "aws_instance" "base" { 
-  ami = "ami-da05a4a0" 
+# requires that the subnet already exists
+resource "aws_instance" "base" {
+  ami           = "ami-0d729a60"
   instance_type = "t2.micro"
+  subnet_id = "subnet-a1d26cea"
 }
-
-#resource "sparkmaster" "base" {
-#  instance = "${aws_instance.base.id}"
-#}  
