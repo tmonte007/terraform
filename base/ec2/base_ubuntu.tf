eca2f3ddeb57c600665dfb1b928b179509a90432
Tom Monte <tmonte@gmail.com>
2017-11-12 14:13:15 -0500

    Rename ubuntu_base.tf to base_ubuntu.tf

---
+++ base/ec2/base_ubuntu.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  shared_credentials_file = "/home/tmonte/.aws/credentials"
+  region     = "us-east-1"
+}
+
+# requires that the subnet already exists
+resource "aws_instance" "base" {
+  ami           = "ami-0d729a60"
+  instance_type = "t2.micro"
+  subnet_id = "subnet-a1d26cea"
+  
+  tags {
+    Name = "tmonte_EC2"
+  }
+}
