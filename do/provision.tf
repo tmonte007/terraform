7aae5805c4d0bf05555057d300647942e6919e59
Tom Monte <tmonte@gmail.com>
2019-07-07 16:30:17 -0400

    Update provision.tf

--- do/provision.tf
+++ do/provision.tf
@@ -2,8 +2,8 @@ provider "digitalocean" {
   token = "${TF_VAR_do_token}"
 }
 
-resource "digitalocean_kubernetes_cluster" "${var.do_cluster}" {
-  name    = "${var.do_cluster}"
+resource "digitalocean_kubernetes_cluster" "do_ny1_k8_1" {
+  name    = "do_ny1_k8_1"
   region  = "${var.do_region}"
   version = "1.14.2-do.0"
 
