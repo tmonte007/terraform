d91284d706190004be406ebc003d044c8e54c083
Tom Monte <tmonte@gmail.com>
2019-07-07 09:01:07 -0400

    Update provision.tf

--- do/provision.tf
+++ do/provision.tf
@@ -1,15 +1,10 @@
-# Digital Ocean token
-variable "do_token" {}
-
-# Configure the DigitalOcean Provider
 provider "digitalocean" {
-  token = "${var.do_token}"
+  token = "${var.TF_VAR_do_token}"
 }
 
-# Create Kubernetes cluster
-resource "digitalocean_kubernetes_cluster" "do_ny1_k8_1" {
-  name    = "do_ny1_k8_1"
-  region  = "nyc1"
+resource "digitalocean_kubernetes_cluster" "${var.do_cluster}" {
+  name    = "${var.do_cluster}"
+  region  = "${var.do_region}"
   version = "1.14.2-do.0"
 
   node_pool {
