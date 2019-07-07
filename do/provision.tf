089ae058368b1ebb34e7bd44dfa439a22b4e5894
Tom Monte <tmonte@gmail.com>
2019-07-07 16:52:21 -0400

    Update provision.tf

--- do/provision.tf
+++ do/provision.tf
@@ -4,12 +4,12 @@ provider "digitalocean" {
 
 resource "digitalocean_kubernetes_cluster" "DOcluster" {
   name    = "DONY1K8S"
-  region  = "nyc1"
-  version = "1.14.2-do.0"
+  region  = "nyc3"
+  version = "1.14.3-do.0"
 
   node_pool {
     name       = "worker-pool"
-    size       = "s-1vcpu-1gb"
+    size       = "s-1vcpu-2gb"
     node_count = 2
   }
 }
