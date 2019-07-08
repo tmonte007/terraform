d9cbabb1fdc618c05799793dd950be1081cfe2f6
Tom Monte <tmonte@gmail.com>
2019-07-07 20:56:00 -0400

    Update provision.tf

--- do/provision.tf
+++ do/provision.tf
@@ -9,3 +9,49 @@ resource "digitalocean_droplet" "web" {
   size   = "s-1vcpu-2gb"
   tags   = ["docker","nyc3"]
 }
+
+resource "digitalocean_firewall" "web" {
+  name = "only-22-80-and-443"
+
+  droplet_ids = ["${digitalocean_droplet.web.id}"]
+
+  inbound_rule {
+      protocol           = "tcp"
+      port_range         = "22"
+      source_addresses   = ["192.168.1.0/24", "2002:1:2::/48"]
+  }
+
+  inbound_rule {
+      protocol           = "tcp"
+      port_range         = "80"
+      source_addresses   = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+      protocol           = "tcp"
+      port_range         = "443"
+      source_addresses   = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+      protocol           = "icmp"
+      source_addresses   = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+      protocol                = "tcp"
+      port_range              = "all"
+      destination_addresses   = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+      protocol                = "udp"
+      port_range              = "all"
+      destination_addresses   = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+      protocol                = "icmp"
+      destination_addresses   = ["0.0.0.0/0", "::/0"]
+  }
+}
